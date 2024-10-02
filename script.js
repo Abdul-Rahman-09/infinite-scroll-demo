588f55cf619ac942b5b3bb832336fd0f4d24db27
Abdul Rahman <abdulrahmanbenyi01@gmail.com>
2024-10-02 16:25:22 +0000

    Photos rendering ensured

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// Globals
+const imageContainer = document.getElementById("image-container");
+let fetchedPhotos = [];
+const loader = document.getElementById("loader");
+
 // Unsplash API
 const photosCount = 15;
 const apiKey = "WDz1zYyXYX_D2o4HehLmDb9CfB_J078vwy-_RVk069k";
@@ -6,12 +11,33 @@ const apiURL = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count
 const getPhotos = async () => {
   try {
     const response = await fetch(apiURL);
-    const data = await response.json();
-    console.log(data);
+    fetchedPhotos = await response.json();
+    // fetchedPhotos = [...fetchedPhotos, ...data];
+    displayPhotos();
   } catch (error) {
     console.log(error);
   }
 };
 
+// Displaying Photos with links to unsplash using forEach
+const displayPhotos = () => {
+  fetchedPhotos.forEach((photo) => {
+    // Create <a> element
+    const linkElement = document.createElement("a");
+    linkElement.setAttribute("href", photo.links.html);
+    linkElement.setAttribute("target", "_blank");
+    // Create <img> element
+    const img = document.createElement("img");
+    img.src = photo.urls.regular;
+    img.setAttribute("alt", photo.alt_description);
+    img.setAttribute("title", photo.alt_description);
+
+    // Arranging elements
+    linkElement.appendChild(img);
+    imageContainer.appendChild(linkElement);
+  });
+};
+
 // On Load
 getPhotos();
+displayPhotos();
